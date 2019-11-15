b70664ee7c8942f1b18df4f6f0c07ace482eaa75
squashgray <squashgray@gmail.com>
2019-11-14 22:22:38 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Forms from "./components/Form";
+import axios from "axios";
+import FormCard from "./components/FormCard";
 import "./App.css";
 
 function App() {
+  const [users, addUser] = useState([]);
+
+  const handleSubmit = (values, tools) => {
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        addUser([...users, res.data]);
+        tools.resetForm();
+      })
+      .catch(err => console.log(err.response));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Forms />
-      </header>
+      <Forms handleSubmit={handleSubmit} />
+      <FormCard users={users} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,31 +2,7 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-// const validate = ({ fname, lname, email, passowrd, tos }) => {
-//   const errors = {};
-
-//   if (!fname) {
-//     errors.fname = "You must input a Name";
-//   } else if (fname.length < 2) {
-//     errors.fname = "ay, ur name too short tho";
-//   }
-
-//   if (!lname) {
-//     errors.lname = "You must input a Name";
-//   } else if (lname.length < 2) {
-//     errors.lname = "ay, ur name too short tho";
-//   }
-
-//   if (!password) {
-//     errors.password = "You must input a Name";
-//   } else if (fname.length < 2) {
-//     errors.password = "ay, ur name too short tho";
-//   }
-
-//   return errors;
-// };
-
-function Forms() {
+function Forms(props) {
   return (
     <div className="form-container">
       <Formik
@@ -37,29 +13,24 @@ function Forms() {
           password: "",
           tos: false
         }}
-        // validate={validate} //
-
         validationSchema={Yup.object({
           fname: Yup.string()
-            .min(2, "Must be at least 2 characters")
+            .min(2, "Must be at least 2 characters!")
             .required("First Name Required"),
           lname: Yup.string()
-            .min(2, "Must be at least 2 characters")
+            .min(2, "Must be at least 2 characters!")
             .required("Last Name Required"),
           email: Yup.string()
             .email("Invalid email address")
             .required("Email Required"),
           password: Yup.string()
-            .min(8, "Password must be at least 8 characters long")
+            .min(8, "Password must be at least 8 characters long!")
             .required("Password required"),
           tos: Yup.boolean()
-            .oneOf([true], "Must accept Terms and Conditions")
+            .oneOf([true], "Must accept Terms and Conditions!")
             .required("required")
         })}
-        onSubmit={(values, tools) => {
-          console.log(values, tools);
-          tools.resetForm();
-        }}
+        onSubmit={props.handleSubmit}
         children={props => {
           return (
             <Form>
